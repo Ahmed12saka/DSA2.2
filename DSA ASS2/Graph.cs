c6a1de5b6b06f50af57b52224049f122cdbd8f46
Ahmed12saka <elsakaa914@gmail.com>
2023-02-20 22:16:32 +0200

    first commit

---
+++ DSA ASS2/Graph.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_ASS2
+{
+    class Graph
+    {
+        private Dictionary<string, Dictionary<string, Tuple<int, int>>> roads;
+
+        public Graph()
+        {
+            roads = new Dictionary<string, Dictionary<string, Tuple<int, int>>>();
+        }
+
+        public void AddCity(string cityName)
+        {
+            if (!roads.ContainsKey(cityName))
+            {
+                roads.Add(cityName, new Dictionary<string, Tuple<int, int>>());
+            }
+        }
+
+        public void AddRoad(string sourceCity, string destCity, int distance, int maxSpeed)
+        {
+            AddCity(sourceCity);
+            AddCity(destCity);
+
+            roads[sourceCity][destCity] = Tuple.Create(distance, maxSpeed);
+        }
+
+        public List<string> ShortestPath(string startCity, string endCity)
+        {
+            Dictionary<string, int> distances = new Dictionary<string, int>();
+            Dictionary<string, string> previous = new Dictionary<string, string>();
+            List<string> unvisited = new List<string>();
+
+            foreach (var city in roads.Keys)
+            {
+                distances[city] = int.MaxValue;
+                previous[city] = null;
+                unvisited.Add(city);
+            }
+
+            distances[startCity] = 0;
+
+            while (unvisited.Count > 0)
+            {
+                string currentCity = null;
+                foreach (var city in unvisited)
+                {
+                    if (currentCity == null || distances[city] < distances[currentCity])
+                    {
+                        currentCity = city;
+                    }
+                }
+
+                if (currentCity == endCity)
+                {
+                    break;
+                }
+
+                unvisited.Remove(currentCity);
+
+                foreach (var neighbor in roads[currentCity])
+                {
+                    int altDistance = distances[currentCity] + neighbor.Value.Item1;
+                    if (altDistance < distances[neighbor.Key])
+                    {
+                        distances[neighbor.Key] = altDistance;
+                        previous[neighbor.Key] = currentCity;
+                    }
+                }
+            }
+
+            List<string> path = new List<string>();
+            string current = endCity;
+            while (current != null)
+            {
+                path.Insert(0, current);
+                current = previous[current];
+            }
+
+            return path;
+        }
+    }
+}
---
+++ DSA ASS2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSA_ASS2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph();
+
+            // Add cities and roads to the graph
+            graph.AddRoad("Varna", "Shumen", 87, 120);
+            graph.AddRoad("Varna", "Dobrich", 48, 100);
+            graph.AddRoad("Varna", "Burgas", 134, 120);
+            graph.AddRoad("Burgas", "Yambol", 102, 120);
+            graph.AddRoad("Yambol", "Sliven", 66, 100);
+            graph.AddRoad("Sliven", "Veliko Trynovo", 78, 120);
+            graph.AddRoad("Veliko Trynovo", "Kazanlyk", 63, 100);
+            graph.AddRoad("Kazanlyk", "Stara Zagora", 79, 120);
+            graph.AddRoad("Stara Zagora", "Tyrgowishte", 109, 120);
+            graph.AddRoad("Tyrgowishte", "Razgrad", 23, 60);
+            graph.AddRoad("Razgrad", "Shumen", 56, 100);
+            graph.AddRoad("Shumen", "Veliko Trynovo", 80, 120);
+
+            // Prompt user to enter the source and destination cities
+            Console.Write("Enter the starting city: ");
+            string startCity = Console.ReadLine();
+            Console.Write("Enter the destination city: ");
+            string endCity = Console.ReadLine();
+
+            // Find the shortest path and display it
+            List<string> path = graph.ShortestPath(startCity, endCity);
+
+            Console.WriteLine("Shortest path from {0} to {1}:", startCity, endCity);
+            Console.WriteLine(string.Join(" -> ", path));
+        }
+    }
+    
+}
